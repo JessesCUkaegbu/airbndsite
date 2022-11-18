ddc6c1213efcd89d5ad1d4288572268e56823c07
JessesCUkaegbu <85634164+JessesCUkaegbu@users.noreply.github.com>
2022-11-18 15:32:44 +0000

    1 binaries uploaded

---
+++ src/component/navbar.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+function Navbar(){
+  return(
+      <div class="navbar">
+         <div class="nav">
+          <p>Logo</p>
+         </div>
+      </div>
+  )
+}
+
+export default Navbar;
