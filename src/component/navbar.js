0d75309f714060e53652df5ec9f3a08e1d9f902e
JessesCUkaegbu <85634164+JessesCUkaegbu@users.noreply.github.com>
2022-11-25 10:19:05 +0000

    1 file modified, 1 added, 1 deleted

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -2,10 +2,12 @@ import React from "react"
 
 
 function Navbar(){
+  const img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2560px-Airbnb_Logo_B%C3%A9lo.svg.png'
+
   return(
       <div class="navbar">
          <div class="nav">
-          <p>Logo</p>
+          <img src={img} alt="" width="160" />
          </div>
       </div>
   )
